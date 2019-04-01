c51245f1238be08a01fccff07de49f42d1b02e6a
github username <github email address>
2019-04-01 13:45:23 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.querySelector("main")
+main.remove()
+
+var newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+
+newHeader.id = 'victory'
+newHeader.innerText = 'Falafel is the champion'
